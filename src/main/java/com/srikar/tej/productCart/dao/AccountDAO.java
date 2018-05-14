ed030cacf8c57c87d7fd9f3aa08648f45dc59c84
Tej Mns <tejmns@gmail.com>
2018-05-14 11:01:54 -0500

    Initial commit

---
+++ src/main/java/com/srikar/tej/productCart/dao/AccountDAO.java
@@ -0,0 +1,22 @@
+package com.srikar.tej.productCart.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.srikar.tej.productCart.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Transactional
+@Repository
+public class AccountDAO {
+ 
+    @Autowired
+    private SessionFactory sessionFactory;
+ 
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+ 
+}
---
+++ src/main/java/com/srikar/tej/productCart/service/UserDetailsServiceImpl.java
@@ -0,0 +1,61 @@
+package com.srikar.tej.productCart.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.srikar.tej.productCart.ProductApplication;
+import com.srikar.tej.productCart.dao.AccountDAO;
+import com.srikar.tej.productCart.entity.Account;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+ 
+    @Autowired
+    private AccountDAO accountDAO;
+ 
+    private static final Logger logger = LogManager.getLogger(ProductApplication.class);
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountDAO.findAccount(username);
+        logger.debug("Account= " + account);
+ 
+        if (account == null) {
+            throw new UsernameNotFoundException("User " //
+                    + username + " was not found in the database");
+        }
+ 
+        // EMPLOYEE,MANAGER,..
+        String role = account.getUserRole();
+ 
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+ 
+        // ROLE_EMPLOYEE, ROLE_MANAGER
+        GrantedAuthority authority = new SimpleGrantedAuthority(role);
+ 
+        grantList.add(authority);
+ 
+        boolean enabled = account.isActive();
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+ 
+        UserDetails userDetails = (UserDetails) new User(account.getUserName(), //
+                account.getEncrytedPassword(), enabled, accountNonExpired, //
+                credentialsNonExpired, accountNonLocked, grantList);
+ 
+        return userDetails;
+    }
+ 
+}
